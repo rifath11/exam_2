996e9eb0ecd88e6f3411b20b5c790de02ebb300f
rifath11 <150188663+rifath11@users.noreply.github.com>
2023-11-28 18:23:48 +0600

    Update question_2.php

--- exam/question_2.php
+++ exam/question_2.php
@@ -2,8 +2,7 @@
 $number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function Even($n) {
-    //return $n % 2 === 0;
-      return !($n & 1);
+    return $n % 2 === 0;
 }
 
 $even_numbers = array_filter($number, 'Even');
